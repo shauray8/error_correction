ca58c29e5b84e6766721700b384060c1cb3c2b5d
Shauray <shauray9@gmail.com>
2022-04-11 14:13:10 +0530

    commnets

---
+++ hamming_code/client.py
@@ -0,0 +1,2 @@
+## getting the bits from the server and then resolving all the errors in the bit 
+import socket 
---
+++ hamming_code/server.py
@@ -0,0 +1,3 @@
+## making this server transfer bits to the client and then using error detection codes
+import socket
+
